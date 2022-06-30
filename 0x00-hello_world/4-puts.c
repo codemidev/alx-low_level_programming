37dd785cb90a6e59802903ba50b7c465ddeb1faf
codemidev <ochaijay81@gmail.com>
2022-06-30 19:48:09 +0100

    Hot fix: code review

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /**
- * Main -Entry point
+ * Main - Entry point
  *
  * Return : Always returns 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
