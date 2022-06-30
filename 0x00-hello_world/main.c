a5b80200a5247659945d255bc8122687c64afc0d
codemidev <ochaijay81@gmail.com>
2022-06-30 20:54:02 +0100

    Hot fix: Betty code style

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,12 @@
-#include<studio.h>
-
+#include<stdio.h>
 
 /**
  * main - entry point
  *
  * Return:  Always 0 (Success)
+ * Description: Prints 0 as return value
  */
 int main(void)
 {
-return (0); 
+	return (0); 
 }
