8fbc47498ed69162fef355e5157e732a62f03038
codemidev <ochaijay81@gmail.com>
2022-07-01 10:28:27 +0100

    Hot fix

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-
 /**
  * main - entry point
  *
  * Return:  Always 0 (Success)
- * Description: Prints 0 as return value
  */
 int main(void)
 {
-	return (0); 
+return (0); 
 }
