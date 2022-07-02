e1937e59665691bced80859915c70a1691c2c1a1
codemidev <ochaijay81@gmail.com>
2022-07-02 09:43:54 +0000

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
-/*
+/**
  * main - Prints random numbers thier corresponding operator state type
  *
  * Return: Always 0(Success)
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	int n = rand() - RAND_MAX/2;
+	n= rand() - RAND_MAX/2;
 
 	if(n > 0)
 	{
