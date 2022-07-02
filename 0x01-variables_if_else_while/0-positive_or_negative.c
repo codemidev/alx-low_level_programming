65456ae2737ea23aa5fcc45f132eecc2c050cb55
codemidev <ochaijay81@gmail.com>
2022-07-02 14:41:08 +0000

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,9 @@
-/*
- * File : 0-positive_or_negative.c
- * Auth: Julius Ochai
- */
-
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
+/* more headers goes there */
 
+/* Betty style doc for function main goes there */
 /**
  * main - Prints random numbers thier corresponding operator state type
  * Return: Always 0(Success)
@@ -17,17 +14,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	
 	if (n > 0)
 	{
-		printf("%i is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n <  0)
 	{
-		printf("%i is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%i is zero\n", n);
+		printf("%d is zero\n", n);
 	}
+	return(0);
 }
+
