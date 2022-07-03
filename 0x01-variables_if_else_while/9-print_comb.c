95a6fd7ff4de07d10a5e90cb80a6be178d5357c3
codemidev <ochaijay81@gmail.com>
2022-07-03 07:25:11 +0000

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,9 +8,11 @@ int main(void)
 {
 	int i = 0;
 
-	for (i = 0; i < 9; i++)
-		putchar(i)
-			putchar(',');
+	for (i = 10; i < 99; i++)
+	{
+		putchar(i);
+		putchar(',');
+	}
 	putchar('\n');
 	return (0);
 }
