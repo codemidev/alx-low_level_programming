40cf176b40b79d7725949cf5d9bc7628d76591e4
codemidev <ochaijay81@gmail.com>
2022-07-06 14:15:24 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints 10 tines alphabets in lower letters
+ * print_alphabet_x10 - Prints 10 tines alphabets in lower letters
  * Description: displays 10 times alphabets in lower cap wite _putchar
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
-	int lw;
+	char lw;
 	int k = 0;
 
 	while (k++ <= 9)
