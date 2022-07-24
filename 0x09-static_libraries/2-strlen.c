1ad8a345dd253ba303e3e907fb8b7464fa186ec0
Jay_codes <ochaijay81@gmail.com>
2022-07-24 01:30:40 -0700

    5-strstr.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: string.
+ *
+ * Return: length.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
