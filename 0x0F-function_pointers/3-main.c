099a1c30899be8ab0b5a05140ce7c387c6e0abd6
Jay_codes <ochaijay81@gmail.com>
2022-08-12 09:50:08 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,37 +4,31 @@
 #include <string.h>
 
 /**
- * main - program that performs simple operations
+ * main - program that performs simple operations
  *
- * @argc: Counts the number of parameters that go into main
+ * @argc: Counts the number of parameters that go into main
  * @argv: Pointer of array of pointers containing strings entering main
- * Return: No element matches -1, if size <=0 -1
- * else index of first element cmp func does not rtrn 0
+ *
+ * Return: No element matches -1, if size <=0 -1
+ * else index of first element cmp func does not rtrn 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int n1, n2, result;
-	int (*p)(int, int);
+	int res;
+	int (*func)(int, int);
 
-	if (argc < 4 || argc > 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+	if (argc != 4)
+		printf("Error\n"), exit(98);
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+	     && argv[2][0] != '/' && argv[2][0] != '%') || strlen(argv[2]) != 1)
+		printf("Error\n"), exit(99);
 
-	p = get_op_func(argv[2]);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
+		printf("Error\n"), exit(100);
 
-	if (p == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	result = p(n1, n2);
-
-	printf("%d\n", result);
+	func = get_op_func(argv[2]);
+	res = func(atoi(argv[1]), atoi(argv[3]));
+	printf("%i\n", res);
 	return (0);
 }
