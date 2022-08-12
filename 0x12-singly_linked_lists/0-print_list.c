482a5c108220b9fca740e5e8810bd27dc47bafaf
Jay_codes <ochaijay81@gmail.com>
2022-08-12 10:19:09 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,26 +2,24 @@
 #include "lists.h"
 
 /**
- * print_list - function that prints all the elements of a list_t list
- * @h: Const pointer of structure list_t
- * Return: Number of nodes
+ * print_list - function with one argument
+ * @h: const pointer type
+ *
+ * Description: prints all the elements of a list_t list
+ * Return: number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
-	unsigned int i = 0;
+	unsigned int count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
-		i++;
+		count++;
 	}
-
-	return (i);
+	return (count);
 }
