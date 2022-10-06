8a667a475bdbeb041a02ee13c3248ce0aed236f1
codemidev <ochaijay81@gmail.com>
2022-10-06 23:14:43 +0100

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an intergal
+ * @n: number to be computed
+ * Return: 0
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
